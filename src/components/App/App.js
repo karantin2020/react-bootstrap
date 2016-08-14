fb0cfad89fb65e8f794552165527ccb216f1cbf5
karantin2020 <karantin3030@mail.ru>
2016-08-14 16:34:09 +0500

    Fix App.js logic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -9,10 +9,44 @@ import generateActions from '../../Actions/immutableActions';
 const name = generateActions(["name"], store.dispatch);
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.def_name = props.name;
+  };
+
+  checkConnection() {
+    var request = new window.XMLHttpRequest({mozSystem: true});
+    request.open('HEAD', 'http://localhost:3000', true);
+    request.timeout = 5750;
+
+    request.addEventListener('load', function(event) {
+       console.log('We seem to be online!');
+    });
+
+    var offlineAlert = function(event) {
+       console.log('We are likely offline');
+    };
+
+    request.addEventListener('error', offlineAlert);
+    request.addEventListener('timeout', offlineAlert);
+
+    request.send(null);
+  };
+
+  componentWillMount() {
+    // this.interval = setInterval(this.checkConnection, 30000);
+  };
+
+  componentWillUnmount() {
+      // clearInterval(this.interval);  
+  };
+
   updateName = (e) => {
+    var new_name = e.target.value || this.def_name;
     name.setIn(
       new Promise(function(resolve, reject) {
-        resolve(e.target.value || "Huray");
+        resolve(new_name);
       })
     );
   };
@@ -23,7 +57,7 @@ class App extends Component {
         <p>Hi, {this.props.name}!</p>
         <p>Yeppeee me!!!</p>
         <p><img src={batteryImg} alt="empty" width="64" /></p>
-        <p><input onChange={this.updateName} placeholder="Place text here" /></p>
+        <p><input onChange={this.updateName} placeholder="Place text here" autoFocus/></p>
       </div>
     );
   }
